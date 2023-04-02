2e401d4884b5f8ef2cf4aff92c6bbb1cc611f477
SergeyWinogradow <123956413+SergeyWinogradow@users.noreply.github.com>
2023-04-02 18:35:01 +0300

    Add files via upload

---
+++ news/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Author, Category, Post, PostCategory, Comment, News
+
+
+admin.site.register(Author)
+admin.site.register(News)
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(PostCategory)
+admin.site.register(Comment)
